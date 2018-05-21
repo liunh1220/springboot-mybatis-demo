8b356d4e3e94ab2e28cfa8c18586e53fe7cc9395
USER20180311UQ\Administrator <liunh120@sina.cn>
2018-05-21 23:21:11 +0800

    修改分表

--- src/main/java/com/example/demo/config/BaseConfiguration.java
+++ src/main/java/com/example/demo/config/BaseConfiguration.java
@@ -4,14 +4,23 @@ import com.dangdang.ddframe.rdb.sharding.jdbc.core.datasource.ShardingDataSource
 import com.example.demo.base.DataSourceNames;
 import com.example.demo.config.ds.MultipleDataSource;
 import com.example.demo.config.ds.ShardingDataSourceCollector;
-import com.example.demo.config.ds.sharding.read.ReadShardingDataSourceFactoryBean;
 import com.example.demo.config.ds.sharding.write.RecordShardingDataSourceFactoryBean;
 import com.example.demo.constant.ApplicationConstant;
+import com.example.demo.util.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
+import okhttp3.OkHttpClient;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.refresh.ContextRefresher;
+import org.springframework.cloud.context.scope.refresh.RefreshScope;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -19,6 +28,8 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
+
+import static com.example.demo.util.Constants.RAW_REST_TEMPLATE;
 
 /**
  * Created by liulanhua on 2018/3/19.
@@ -42,6 +53,58 @@ public class BaseConfiguration extends WebMvcConfigurerAdapter  {
     }
 
     @Bean
+    public ApplicationContextHolder applicationContextHolder(ApplicationConstant applicationConstant) {
+        ApplicationContextHolder applicationContextHolder = ApplicationContextHolder.getInstance();
+        applicationContextHolder.init(applicationConstant);
+        return applicationContextHolder;
+    }
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        return JacksonUtils.mapper;
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        return new MappingJackson2HttpMessageConverter(objectMapper());
+    }
+
+    @Primary
+    @Bean
+    public okhttp3.OkHttpClient okHttpClient(ApplicationConstant applicationConstant){
+        return OkHttpUtils.okHttpClientBuilder(applicationConstant).build();
+    }
+
+    @LoadBalanced
+    @Bean
+    @Primary
+    public RestTemplate restTemplate(MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter,
+                                     OkHttpClient okHttpClient) {
+        return EnhancedRestTemplate.assembleRestTemplate(mappingJackson2HttpMessageConverter, okHttpClient);
+    }
+
+    @Bean(name = RAW_REST_TEMPLATE)
+    public RestTemplate rawRestTemplate(MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter,
+                                        OkHttpClient okHttpClient) {
+
+        return EnhancedRestTemplate.assembleRestTemplate(mappingJackson2HttpMessageConverter, okHttpClient);
+    }
+
+    @Bean
+    public RefreshScope scope(){
+        return new RefreshScope();
+    }
+
+    @Bean
+    public ContextRefresher contextRefresher(ConfigurableApplicationContext context,
+                                             RefreshScope scope) {
+        return new MyContextRefresher(context, scope);
+    }
+
+
+
+    @Bean
     public MultipleDataSource multipleDataSource() {
         return new MultipleDataSource(Lists.newArrayList(DataSourceNames.DB_READ, DataSourceNames.DB_WRITE));
     }
@@ -49,15 +112,20 @@ public class BaseConfiguration extends WebMvcConfigurerAdapter  {
     @Bean
     @Qualifier(DataSourceNames.SHARDING_RP_WRITE)
     public RecordShardingDataSourceFactoryBean recordShardingDataSource() {
-        return new RecordShardingDataSourceFactoryBean();
+        return new RecordShardingDataSourceFactoryBean(Boolean.TRUE);
     }
-
 
     @Bean
     @Qualifier(DataSourceNames.SHARDING_RP_READ)
+    public RecordShardingDataSourceFactoryBean readShardingDataSource() {
+        return new RecordShardingDataSourceFactoryBean(Boolean.FALSE);
+    }
+
+    /*@Bean
+    @Qualifier(DataSourceNames.SHARDING_RP_READ)
     public ReadShardingDataSourceFactoryBean readShardingDataSource() {
         return new ReadShardingDataSourceFactoryBean();
-    }
+    }*/
 
     @Bean
     public ShardingDataSourceCollector readShardingDataSourceCollector(
